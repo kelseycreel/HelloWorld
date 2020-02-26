e229f74783fb2d01c53c8a282a39f08aba9a2fbc
Kelsey Creel <kelseycreel@gmail.com>
2020-02-25 18:07:45 -0600

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Howdy Y'all!!");
+            Console.ReadKey(); // What happens if you run the app without this line?
+        }
+    }
+}
