43671a810fdfbbe9b8b3b74e53ebc3f9473dddee
Kelsey Creel <kelseycreel@gmail.com>
2020-02-25 19:24:01 -0600

    all done

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,8 +6,57 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Howdy Y'all!!");
-            Console.ReadKey(); // What happens if you run the app without this line?
+            //// console greeting
+            //Console.WriteLine("Howdy Y'all!!");
+            //Console.ReadKey(); // What happens if you run the app without this line?
+
+            //// animal syllables 
+            //var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan", "Bat" };
+            //var vowels = new[] { 'a', 'e', 'i', 'o', 'u', 'y' };
+            //foreach (var animal in animals)
+            //{
+            //    var countOfVowels = 0;
+            //    foreach (var animalChar in animal)
+            //    {
+            //        foreach (var vowel in vowels)
+            //        {
+            //            if (vowel == animalChar)
+            //            {
+            //                countOfVowels++;
+            //            }
+            //        }
+            //    }
+            //    if (countOfVowels >= 2)
+            //    {
+            //        Console.WriteLine(animal);
+            //        Console.ReadLine();
+            //    }
+            //}
+
+            // command line arguments
+            //var arguments = new []{ "Southern", "British", "Australian"};
+
+            Console.WriteLine($"Choose a dialect: {args[0]}, {args[1]}, {args[2]}");
+            var dialect = Console.ReadLine();
+            var otherDialect = false;
+            foreach (var argument in args)
+            {
+                if (argument.ToLower() == dialect.ToLower())
+                {
+                    Console.WriteLine($"Your dialect: {dialect}");
+                    otherDialect = false;
+                    break;
+                }
+                else
+                {
+                    otherDialect = true;
+                }
+            } 
+            if (otherDialect)
+            {
+                Console.WriteLine("Oops! You have to choose one of MY dialects!");
+            }
+            Console.ReadLine();
         }
     }
 }
